f2a734762561d3ed582302eec76dbf2fb521838e
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-05-20 21:16:21 -0300

    feat: implement responsive header component with navigation menu and mobile sheet
    - Added Header component with logo, navigation links, and mobile menu functionality.
    - Integrated NavigationMenu and Select components for better navigation experience.
    - Created reusable UI components for navigation, select, and sheet functionalities with Shadcn components.
    - Implemented utility function for class name merging.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { type FC } from 'react';
-const App: FC = () => {
-  return <h1 className="bg-amber-100">Hello World</h1>;
-};
+import Header from './components/Header/index'
+const App = () => {
+  return <Header />
+}
 
-export default App;
+export default App
